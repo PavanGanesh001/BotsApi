cce1f9d2ad129b45b4f9b36c0925b04be74d9c81
praveen G <37699745+Gpraveen12345@users.noreply.github.com>
2024-02-16 11:06:13 +0530

    Fixed UI alignments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
-import { HomeComponent } from './home/home.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HomeComponent } from './home/home.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FooterComponent } from './footer/footer.component';
-import { DetailComponent } from './detail/detail.component';
 import { NgxMarqueeModule } from 'ngx-marquee';
+import { DetailComponent } from './detail/detail.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, FooterComponent, DetailComponent],
